ef766bf5cc67c217fb2f9b5848ede89af33acdc6
Mohammed <humana.human@gmail.com>
2018-06-30 03:34:05 +0300

    Created login page without javascript enabled

--- learningCenterfrontend/src/app/app.module.ts
+++ learningCenterfrontend/src/app/app.module.ts
@@ -7,18 +7,21 @@ import { HttpClientModule } from '@angular/common/http'
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   { path:"", redirectTo:"home", pathMatch:"full" },
-  { path:"home", component:HomeComponent }
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
