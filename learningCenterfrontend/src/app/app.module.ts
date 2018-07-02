d26b3dd2879740f150ee650ab59b4d8b437d0465
Mohammed <humana.human@gmail.com>
2018-07-03 01:30:51 +0300

    Coppied register page from old project

--- learningCenterfrontend/src/app/app.module.ts
+++ learningCenterfrontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
   { path:"", redirectTo:"home", pathMatch:"full" },
   { path: "home", component: HomeComponent },
-  { path: "login", component: LoginComponent }
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }
 ]
 
 @NgModule({
@@ -21,7 +23,8 @@ const routes: Routes = [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
