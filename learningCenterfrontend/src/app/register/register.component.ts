4b4a5f962529034f479d16b8c079d33817b7c6d0
Mohammed Yousif <humana.human@gmail.com>
2018-07-30 00:40:42 +0300

    Copy register javascript code from old project

--- learningCenterfrontend/src/app/register/register.component.ts
+++ learningCenterfrontend/src/app/register/register.component.ts
@@ -11,11 +11,12 @@ export class RegisterComponent implements OnInit {
   constructor(public helperService: HelperService) { }
 
   ngOnInit() {
-    this.initSemanticUIFormValidationForRegister()
+    this.initSemanticUIFormValidationForRegister(this.registerFormSubmitHandler)
   }
 
-  testtttt() {
+  registerFormSubmitHandler() {
     console.log("done")
+    return false
   }
 
   //upon calling this function it clicks on selecting image input
@@ -24,94 +25,91 @@ export class RegisterComponent implements OnInit {
     el.click();
   }
 
-  initSemanticUIFormValidationForRegister() {
-    /*$(document)
+  initSemanticUIFormValidationForRegister(onSuccessCallBack) {
+    $(document)
     .ready(function() {
-    $('.ui.form.register')
-      .form({
-      fields: {
-          email: {
-              identifier  : 'email',
-              rules: [
-                  {
-                  type   : 'empty',
-                  prompt : 'Please enter your e-mail'
-                  },
-                  {
-                  type   : 'email',
-                  prompt : 'Please enter a valid e-mail'
-                  }
-              ]
-          },
-          password: {
-              identifier  : 'password',
-              rules: [
-                  {
-                  type   : 'empty',
-                  prompt : 'Please enter your password'
-                  },
-                  {
-                  type   : 'length[6]',
-                  prompt : 'Your password must be at least 6 characters'
-                  }
-              ]
-          },
-          passwordRepeat: {
-              identifier  : 'passwordRepeat',
-              rules: [
-                  {
-                  type   : 'empty',
-                  prompt : 'Please re-enter your password'
-                  },
-                  {
-                  type   : 'match[password]',
-                  prompt : 'Password did not match'
-                  }
-              ]
-          },
-          firstname: {
-              identifier: 'name1',
-              rules: [
-                  {
-                  type   : 'empty',
-                  prompt : 'Please enter your first name'
-                  },
-                  {
-                  type   : 'length[2]',
-                  prompt : 'Your first name must be at least [2] characters'
-                  }
-              ]
-          },
-          secondname: {
-              identifier: 'name2',
-              rules: [
-                  {
-                  type   : 'empty',
-                  prompt : 'Please enter your second name'
-                  },
-                  {
-                  type   : 'length[2]',
-                  prompt : 'Your second name must be at least [2] characters'
-                  }
-              ]
-          },
-          userImg: {
-              identifier: 'userImg',
-              rules: [
-                  {
-                  type   : 'empty',
-                  prompt : 'Please select an image'
-                  }
-              ]
-          }
-      },onSuccess: function() {
-        alert('correct!');
-          this.testtttt()
-          alert('correct!');
-          return false;
-      }
-      });
-    });*/
+
+        (<any>$('.ui.form.register'))
+        .form({
+        fields: {
+            email: {
+                identifier  : 'email',
+                rules: [
+                    {
+                    type   : 'empty',
+                    prompt : 'Please enter your e-mail'
+                    },
+                    {
+                    type   : 'email',
+                    prompt : 'Please enter a valid e-mail'
+                    }
+                ]
+            },
+            password: {
+                identifier  : 'password',
+                rules: [
+                    {
+                    type   : 'empty',
+                    prompt : 'Please enter your password'
+                    },
+                    {
+                    type   : 'length[6]',
+                    prompt : 'Your password must be at least 6 characters'
+                    }
+                ]
+            },
+            passwordRepeat: {
+                identifier  : 'passwordRepeat',
+                rules: [
+                    {
+                    type   : 'empty',
+                    prompt : 'Please re-enter your password'
+                    },
+                    {
+                    type   : 'match[password]',
+                    prompt : 'Password did not match'
+                    }
+                ]
+            },
+            firstname: {
+                identifier: 'name1',
+                rules: [
+                    {
+                    type   : 'empty',
+                    prompt : 'Please enter your first name'
+                    },
+                    {
+                    type   : 'length[2]',
+                    prompt : 'Your first name must be at least [2] characters'
+                    }
+                ]
+            },
+            secondname: {
+                identifier: 'name2',
+                rules: [
+                    {
+                    type   : 'empty',
+                    prompt : 'Please enter your second name'
+                    },
+                    {
+                    type   : 'length[2]',
+                    prompt : 'Your second name must be at least [2] characters'
+                    }
+                ]
+            },
+            userImg: {
+                identifier: 'userImg',
+                rules: [
+                    {
+                    type   : 'empty',
+                    prompt : 'Please select an image'
+                    }
+                ]
+            }
+        },onSuccess: onSuccessCallBack
+        });
+        
+    });
   }
 
 }
