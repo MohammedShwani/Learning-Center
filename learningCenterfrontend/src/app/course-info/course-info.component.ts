f313265d878d9271ee5bdd1c5678dc69beb4bdbc
Mohammed Yousif <humana.human@gmail.com>
2018-08-04 02:57:34 +0300

    Modifiy footer + clean course-info component

--- learningCenterfrontend/src/app/course-info/course-info.component.ts
+++ learningCenterfrontend/src/app/course-info/course-info.component.ts
@@ -8,72 +8,30 @@ import { NgxY2PlayerComponent, NgxY2PlayerOptions } from 'ngx-y2-player';
   styleUrls: ['./course-info.component.css']
 })
 export class CourseInfoComponent implements OnInit {
-  YT: any
-  youtubePlayer: any
   youtubePlayerReady = false
-  done = false
-  previewVideoSrc: any
-  previewVideoID = '6wD4V0rvlDI'
-
+  previewVideoID = 'z8WdQsPknf0'
   @ViewChild('video') video: NgxY2PlayerComponent;
+  
   playerOptions: NgxY2PlayerOptions = {
-    videoId: 'z8WdQsPknf0',
-    height: 'auto', // you can set 'auto', it will use container width to set size
+    videoId: this.previewVideoID,
+    height: 'auto',
     width: 'auto',
     resizeDebounceTime: 1,
     playerVars: {
       autoplay: 0,
       fs: 0,
-      rel: 0,
-      origin: "http://localhost:4200"
+      rel: 0
     },
-    // aspectRatio: (3 / 4), // you can set ratio of aspect ratio to auto resize with
   };
 
   constructor(private _sanitizer: DomSanitizer) {}
 
   ngOnInit() {
-
-    this.previewVideoSrc = this._sanitizer.bypassSecurityTrustResourceUrl(
-      this.getFullYoutubeSrcFromVideoID(this.previewVideoID)
-    )
     
   }
 
-  private loadYoutubIFrameApi() {
-    // 2. This code loads the IFrame Player API code asynchronously.
-
-    /*let tag = document.createElement('script')
-    let firstScriptTag = document.getElementById('course-info-container')
-    tag.src = "https://www.youtube.com/iframe_api"
-    firstScriptTag.appendChild(tag)*/
-  }
-
-  private initYoutubPlayer() {
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    /*let windowHandler = (<any>window)
-    windowHandler.onYouTubeIframeAPIReady = () =>  {
-      this.YT = YT
-      this.youtubePlayer = new this.YT.Player('player', {
-        events: {
-          'onReady': (e) => {
-            this.onPlayerReady(e)
-          },
-          'onStateChange': (e) => {
-            this.onPlayerStateChange(e)
-          }
-        }
-      });
-    }*/
-  }
-
-  private getFullYoutubeSrcFromVideoID(videoID: String) {
-    return `https://www.youtube.com/embed/${videoID}?enablejsapi=1&showinfo=0&rel=0&autoplay=0&fs=0`
-  }
-
   // 4. The API will call this function when the video player is ready.
-  onReady(event) {console.log('player ready')
+  onReady(event) {
       this.youtubePlayerReady = true
   }
 
@@ -81,9 +39,7 @@ export class CourseInfoComponent implements OnInit {
   //    The function indicates that when playing a video (state=1),
   //    the player should play for six seconds and then stop.
   onStateChange(event) {
-    /*if (event.data == this.YT.PlayerState.PLAYING && !this.done) {
-      this.done = true
-    }*/
+    
   }
   ///////////////////////////////////////////////////////////////////////////
 
